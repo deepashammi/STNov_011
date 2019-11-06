48923215b95c931995320f507c18c712baf47166
DeepaShammi <deepa.shammi@aurea.com>
2019-11-06 08:41:20 +0530

    Create X.java

---
+++ a/b/X.java
@@ -0,0 +1,17 @@
+package a.b;
+
+public class X {
+    public void y() {
+        int abc = 10;
+        int cde = abc * 5;
+        abc = cde + 7;
+        abc=abc+1;
+    }
+{
+    public void z() {
+        int abc = 108;
+        int cde = abc * 5;
+        abc = cde + 7;
+        abc=abc+12;
+
+   }
